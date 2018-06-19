87d639cbacfbe8f8d715fc93a20177f4c840e69d
Leonardo sirio <>
2018-06-18 21:52:52 -0300

    Meu primeiro envio

---
+++ PhpProject1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="Leonardo.php">Clique aqui para ir para a página do leonardo</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
